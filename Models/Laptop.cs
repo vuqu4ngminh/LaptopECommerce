3e2dc67dd2c8c0798a9b904265fdf82727891031
vuqu4ngminh <solution.vuqu4ngminh@gmail.com>
2023-10-03 01:33:07 +0700

    Added Laptop Model

---
+++ Models/Laptop.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LaptopECommerce.Models
+{
+    public class Laptop
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
