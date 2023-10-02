bae69346408a292d51d2ca708311e4183e1bf359
vuqu4ngminh <solution.vuqu4ngminh@gmail.com>
2023-10-02 17:12:33 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaptopECommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
